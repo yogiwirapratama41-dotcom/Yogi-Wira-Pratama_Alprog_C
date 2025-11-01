dd38d6091cefce6be0eb4f1072bf29cf119a0600
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-11-01 21:09:04 +0700

    Add files via upload

---
+++ meet.py
@@ -0,0 +1,4 @@
+print("Nama Yogi Wira Pratama ")
+print("Anak kedua")
+print("Seorang kakak berharap adiknya jauh lebih baik dari dirinya")
+print("Jangan sia-siakan kuliahmu, Orang tuamu relabl banting tulang demi anaknya mendapatkan gelar.")
