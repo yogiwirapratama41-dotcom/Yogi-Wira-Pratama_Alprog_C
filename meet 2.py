e0a5940f2eab63bbd02f83fc36046f5ddc374cf5
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-11-06 16:11:30 +0700

    Add files via upload

---
+++ meet 2.py
@@ -0,0 +1,30 @@
+#Variable adalah cara menyimpan nilai atau value (aljabar)
+#x: variable =: assigment 13: nilai (x=3)
+# seperti
+x = 13
+y = 20
+lebar = 30
+print("niali x adalah :", x)
+print("niali y adalah :", y)
+print("niali lebar adalah :", lebar)
+# ATURAN PENAMAAN VARIABLE
+#tidak boleh diawali angka
+bet99 = 2311
+print("nilai dari bet99 adalah :", bet99)
+#jika lebih dari satu kata, gabung dengan_
+Raden_wira = 1006
+print("niali dari Raden_wira :", Raden_wira)
+#bisa juga menggabungkan dengan huruf kpital
+RadenWira = 2511
+print("niali dari RadenWira :", RadenWira)
+# PEMENGGALAN INDERECT
+x = 1212
+z = x
+print("niali dari z :",z)
+# tugas
+# nama saya adalah = (nama anda)
+# nim saya adalah = (nim anda)
+a = "Yogi Wira Pratama"
+b = 25241085
+print("nama saya adalah :", a)
+print("nim saya adalah :", b)
