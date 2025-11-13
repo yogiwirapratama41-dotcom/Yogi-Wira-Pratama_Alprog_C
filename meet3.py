e46d3cf4aaf2ad680a740fcf0b69f642b30f20bd
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-11-13 14:51:43 +0800

    Add files via upload

---
+++ meet3.py
@@ -0,0 +1,28 @@
+# TIPE DATA 
+#  (tipe dari sebuah nilai)
+
+# tpe data integer
+x = 10
+print("tipe data dari x =",type(x))
+
+# tipe data float
+y = 5.5
+print("tipe data dari y =",type(y))
+
+# tipe data string
+nama = "Raden wira"
+print("tipe data dari nama =", type(nama))
+
+# tipe data bolean
+
+kebenaran = True
+print("nilai dari kebenaran=", type (kebenaran))
+print("tipe data dari kebenaran=", type (kebenaran))
+
+#HASIL DARI NILAI
+
+print("tipe data dari x =", x)
+print("tipe data dari y =", y)
+print("tipe data dari nama =", nama)
+print("nilai dari kebenaran=", kebenaran)
+print("tipe data dari kebenaran=",kebenaran)
