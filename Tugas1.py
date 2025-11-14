596749b6376d1481fa96ed493e6f8ecce3f8b7f1
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-11-14 15:59:21 +0800

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,19 @@
+Nama = "Yogi Wira Pratama"
+print("Nama saya adalah =", Nama)
+
+Umur = 19
+print("umur saya adalah =", Umur)
+
+
+Nama = input("Masukkan nama Anda : ")
+print("Salam kenal ", Nama)
+print(type(Nama))
+
+print("Tipe data dari Nama anda =", type(Nama))
+ 
+
+Umur = int (input ("Masukkan umur Anda : "))
+print("wah ternyata umur anda =", Umur)
+print(type(Umur))
+
+print("Tipe data dari umur anda =", type(Umur))
