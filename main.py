62973d2b0cb3be4399738eac1df3c43401bafc87
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-11-14 15:48:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# MENGAMBIL DATA DARI USER
+
+Nama = input("Masukkan nama Anda : ")
+print("Selamat Datang", Nama)
+print(type(Nama))
+
+# Jika ingin mengambil data INTEGER, maka contohnya seperti di bawah ini
+
+Data_int = int (input ("Masukkan data Anda : "))
+print("Nilai Anda =", Data_int)
+print(type(Data_int))
+
+# Jika ingin mengambil data FLOAT, maka contohnya seperti di bawah ini
+
+Data_float = float (input ("Masukkan data Anda : "))
+print("Nilai Anda =", Data_float)
+print(type(Data_float))
+
+# Jika ingin mengambil data BLOOEAN, maka contohnya seperti di bawah ini
+
+Data_bool = bool (input ("Masukkan data Anda : "))
+print("Nilai Anda =", Data_bool)
+print(type(Data_bool))
