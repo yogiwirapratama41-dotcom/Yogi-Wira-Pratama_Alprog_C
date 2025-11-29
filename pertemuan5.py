3d4de2bedb4c7705113f49f554881359658a8f7c
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-11-29 16:16:31 +1000

    Add files via upload

---
+++ pertemuan5.py
@@ -0,0 +1,58 @@
+# operasi aritmatika
+# x / + -
+
+# penjumlahan
+a = 2
+b = 2
+nilai = a+b
+print(a,"+", b, "=", nilai)
+
+# pengurangan
+a = 2
+b = 1
+nilai = a-b
+print(a,"-", b, "=", nilai)
+
+# perkalian
+a = 2
+b = 3
+nilain = a*b
+print(a,"x", b, "=", nilai)
+
+# pembagian
+a = 2
+b = 1
+nilai = a/b
+print(a,"/", b, "=", nilai)
+
+# perpangkatan
+a = 6
+b = 2
+nilai = a**b
+print(a,"^", b, "=", nilai)
+
+# modulus (%)
+a = 4
+b = 2
+nilai = a%b
+print(a,"%", b, "=", nilai)
+
+# floor division (//)
+a = 23
+b = 11
+nilai = a//b
+print(a, "//", b, "=", nilai)
+
+# operasi prioritas
+# 1. ()
+# 2. perpangkatan akar
+# 3. x /
+# 4. + -
+
+# penjumlahan (+)
+# pengurangan (-)
+# perkalian (*)
+# pembagian (/)
+# perpangkatan (**)
+# modulus (%) (sisa bagi)
+# floor division (//) (pembualan)
