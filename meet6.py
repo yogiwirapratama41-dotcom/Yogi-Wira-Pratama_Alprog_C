ac413176f71a4f1cb1d57d874f054d00586558f9
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-12-06 23:17:04 +1000

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,23 @@
+# Nama : Yogi Wira Pratama
+# NIM : 25241085
+# Latihan airtmatika, Aplikasi konversi suhu
+# Buatlah program konversi suhu dengan output sebagai berikut :
+
+
+# suhu anda adalah
+# suhu anda dalam fahrenheit
+# suhu anda dalam reamur
+# suhu anda dalam kelvin
+
+
+Celcius = float(input("masukkan suhu anda dalam celcius : "))
+fahrenheit = celcius * 9/5 + 32
+print("suhu anda dalam fahrenheit :", fahren)
+
+Celcius = float(input("masukkan suhu anda dalam celcius : "))
+reamur = celcius * 4/5 
+print("suhu anda dalam reamur :", reamur)
+
+Celcius = float(input("masukkan suhu anda dalam celcius :"))
+kelvin = celcius + 462.11
+print("suhu anda dalam kelvin :",  kelvin)
