d0269128bd3789875c31743e74a4e7eedab6940d
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2025-12-13 02:40:24 +1000

    Add files via upload

---
+++ meet7.py
@@ -0,0 +1,58 @@
+# komparasi = true / false 
+# > < >= <= != == is is-not
+
+# komparasi dari (>) = lebih besar
+print("====== lebih dari (>)")
+x = 25
+y = 85
+hasil = x > 8
+print(x, ">", 8, "=", hasil)
+
+# komparasi dari (<) = lebih kecil
+print("====== kurang dari (<)")
+x = 25
+y = 85
+hasil = x < 8
+print(x, "<", 8, "=", hasil)
+
+# komparasi dari (>=) = lebih besar dari
+print("====== lebih besar dari sama dengan (>=)")
+x = 25
+y = 85
+hasil = x >= 8
+print(x, ">=", 8, "=", hasil)
+
+# komparasi dari (<=) = lebih kecil dari
+print("====== kurang dari sama dengan (<=)")
+x = 25
+y = 85
+hasil = x <= 8
+print(x, "<=", 8, "=", hasil)
+
+# komparasi dari (!=) = tidak sama dengan
+print("====== tidak sama dengan (!=)")
+x = 25
+y = 85
+hasil = x != 8
+print(x, "!=", 8, "=", hasil)
+ 
+# komparasi dari (==) = perbandingan
+print("====== sama dengan sama dengan (==)")
+x = 25
+y = 85
+hasil = x == 8
+print(x, "==", 8, "=", hasil)
+
+# komparasi dari (is) = perbandingan
+print("====== perbandingan (is)")
+x = 25
+y = 85
+hasil = x is 8
+print(x, "is", 8, "=", hasil)
+
+# komparasi dari (is not)
+print("====== tidak sama dengan (is not)")
+x = 25
+y = 85
+hasil = 8 is not 8
+print(x, "is not", 8, "=", hasil)
